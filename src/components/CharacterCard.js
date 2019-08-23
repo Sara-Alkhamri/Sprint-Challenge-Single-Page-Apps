996100a1bc9a69bba7e3b750350e9877ee590a4a
Sara Alkhamri <s.alkhamri@gmail.com>
2019-08-23 09:45:59 -0700

    Created AppRouter to test components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 
+
 export default function CharacterCard() {
   return (
-    <div>
+    <StyledDiv>
       <img src={props.character.image} alt={props.character.name} />
       <h2>Name: {props.character.name}</h2>
-    </div>
+    </StyledDiv>
   )
 }
