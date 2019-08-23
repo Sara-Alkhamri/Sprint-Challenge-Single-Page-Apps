18dce6c2297b0fda72f1f20b41c4b79feeee4999
Sara Alkhamri <s.alkhamri@gmail.com>
2019-08-23 08:51:18 -0700

    Imported axios. Added useState and API request and pushing untracked file

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [data, setDdata] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get (`https://rickandmortyapi.com/api/character/`)
+      .then(reonse => {
+        setDdata(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }, []);
 
   return (
-    <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    // <section className="character-list grid-view">
+    //   <h2>TODO: `array.map()` over your state here!</h2>
+    // </section>
+    <div>
+      {data.map(obj => {
+        return <CharacterCard key ={obj.id} character={obj} />
+      })}
+    </div>
   );
 }
