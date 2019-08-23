40ff34c3ce09df13526c360af159a9ad48e010c5
Sara Alkhamri <s.alkhamri@gmail.com>
2019-08-23 09:39:42 -0700

    Created LocationCard and LocationList

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+
+export default function LocationCard(props) {
+  // return <span>todo: location</span>;
+  return (
+    <div className="locationCard">
+    <h2>Name: {removeProperties.location.name}</h2>
+    <p>Type: {props.location.type}</p>
+    <p>Dimension: {props.location.dimension}</p>
+    <p>Residents:{props.location.residents.length}</p>
+    </div>
+  )
 }
